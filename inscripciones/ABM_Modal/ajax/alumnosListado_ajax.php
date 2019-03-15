5c5bef54d2e190180a8a5e5d3da13cb729ddc8c1
Tinker <elianalucila@gmail.com>
2019-03-15 00:23:11 -0300

    wip pasar todo a php

---
+++ inscripciones/ABM_Modal/ajax/alumnosListado_ajax.php
@@ -0,0 +1,211 @@
+
+<?php require_once('../../Connections/MySQL.php'); ?>
+<?php
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// verify that the user is admin
+if ($_SESSION['MM_UserGroup'] != 'Admin') {
+    die("No cuenta con permisos suficientes");
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
+?>
+
+
+<?php
+
+	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
+	if($action == 'ajax'){
+		$reload = 'Equivalencias.php';
+		//consulta principal para recuperar los datos
+
+    function getSubject($id) {
+      $query = "select * from terciario.materias_plan where IdMateria={$id}";
+      $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
+      $subject = mysqli_fetch_assoc($recordset);
+      return $subject;
+    }
+
+    function getSubjectDetails($id) {
+      $query = "select * from terciario.materias where IdMateria={$id}";
+      $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
+      $subjectDetails = mysqli_fetch_assoc($recordset);
+      return $subjectDetails;
+    }
+
+    function getSubjectCorrelatives($subject) {
+      $correlatives = [];
+
+      $query = "select * from terciario.correlativas
+                where (IdMateriaPlan={$subject['IdMateriaPlan']})";
+      $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
+      $result = mysqli_fetch_all($recordset, MYSQLI_ASSOC);
+
+      foreach($result as $correlative_result) {
+        array_push($correlatives, getSubject($correlative_result['IdCorrelativa']));
+      }
+
+      return $correlatives;
+    }
+
+    function getStudents() {
+      $query_students = "select Apellido, Nombre, DNI, IdAlumno from terciario.alumnos";
+      $recordset_students = mysqli_query(dbconnect(),$query_students) or die(mysqli_error());
+      $students = mysqli_fetch_all($recordset_students, MYSQLI_ASSOC);
+
+      return $students;
+    }
+
+    function studentHasSign($student, $subject) {
+      $hasSign = False;
+
+      $query = "select * from terciario.alumno_materias
+                where (IdAlumno={$student['IdAlumno']} and IdMateriaPlan={$subject['IdMateriaPlan']})";
+      $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
+      $result = mysqli_fetch_assoc($recordset);
+
+      if (isset($result['FechaFirma'])) {
+        $hasSign = True;
+      }
+
+      return $hasSign;
+    }
+
+    function studentHasCorrelatives($student, $subject_correlatives) {
+      $has_correlatives = True;
+
+      foreach($subject_correlatives as $correlative) {
+        if (!studentHasSign($student, $correlative)) {
+          $has_correlatives = False;
+          break;
+        }
+      }
+
+      return $has_correlatives;
+      }
+
+    $materia_id = $_REQUEST['materia'];
+    $subject = getSubject($materia_id);
+    $subject_correlatives = getSubjectCorrelatives($subject);
+
+
+      $allowed_student =[];
+
+      foreach (getStudents() as $student) {
+        if (!studentHasSign($student, $subject) and
+          studentHasCorrelatives($student, $subject_correlatives)) {
+
+          array_push($allowed_student, $student);
+        }
+      }
+
+    if(isset($_SESSION['listado'])) {
+      $_SESSION['listado'] = $allowed_student;
+    }
+
+    #print_r($allowed_student);
+    $subjectDetails = getSubjectDetails($materia_id);
+
+		if ($allowed_student) {
+      asort($allowed_student);
+      foreach ($allowed_student as $student): ?>
+      <tr>
+        <td width="500"> <?php echo '<pre>' . print_r($allowed_student) . '</pre>'; ?></td>
+        <td width="150"  align="center"><h4><?php echo $student['DNI']; ?></h4></td>
+        <td width="400"  align="center"><h4><?php echo $student['Apellido'] . " " . $student['Nombre']; ?></h4></td>
+        <td idth="700" align="center"><h4></h4></td>
+        <td width="100" align="center" class="actions">
+          <BR>
+        <button class="quitarBtn" data-alumno-id=<?php echo $student['IdAlumno']; ?>>Quitar</button>
+        </td>
+      </tr>
+    <?php endforeach;
+
+		} else {
+			?>
+			<div class="alert alert-warning alert-dismissable">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+              <h4>Aviso!!!</h4> No hay datos para mostrar
+            </div>
+			<?php
+		}
+	} elseif ($action == 'borrar') {
+    function DeleteAlumnoFromResult($listado, $id) {
+      foreach ($listado as $key => $val) {
+        if ($val['IdAlumno'] === $id) {
+          unset($listado[$key]);
+        }
+      }
+      return $listado;
+    }
+
+    if(isset($_REQUEST['id'])) {
+      $_SESSION['listado'] = DeleteAlumnoFromResult($_SESSION['listado'],$_REQUEST['id']);
+    }
+
+    if ($_SESSION['listado']) {
+      asort($_SESSION['listado']);
+      foreach ($_SESSION['listado'] as $student): ?>
+      <tr>
+        <td width="500"> <?php echo '<pre>' . print_r($_SESSION['listado']) . '</pre>'; ?></td>
+        <td width="150"  align="center"><h4><?php echo $student['DNI']; ?></h4></td>
+        <td width="400"  align="center"><h4><?php echo $student['Apellido'] . " " . $student['Nombre']; ?></h4></td>
+        <td idth="700" align="center"><h4></h4></td>
+        <td width="100" align="center" class="actions">
+          <BR>
+        <button class="quitarBtn" data-alumno-id=<?php echo $student['IdAlumno']; ?>>Quitar</button>
+        </td>
+      </tr>
+    <?php endforeach;
+
+		}
+  } elseif ($action == 'agregar') {
+    function AddAlumnoFromResult($listado, $student) {
+      $allowed_students = $listado;
+      $student_in_allowed_students = FALSE;
+
+      // print_r($allowed_students); print_r($student);
+      foreach($allowed_students as $allowed_student) {
+        if ($allowed_student["IdAlumno"] == $student["IdAlumno"]) {
+          $student_in_allowed_students = TRUE;
+        }
+      }
+
+      if (!$student_in_allowed_students) {
+        $allowed_students[] = $student;
+      }
+
+      return $allowed_students;
+    }
+
+    if(isset($_REQUEST['agregarAlumno'])) {;
+      $student = $_REQUEST['agregarAlumno'];
+
+      $_SESSION['listado'] = AddAlumnoFromResult($_SESSION['listado'], $student);
+    }
+
+    if ($_SESSION['listado']) {
+      asort($_SESSION['listado']);
+      foreach ($_SESSION['listado'] as $student): ?>
+      <tr>
+        <td width="500"> <?php echo '<pre>' . print_r($_SESSION['listado']) . '</pre>'; ?></td>
+        <td width="150"  align="center"><h4><?php echo $student['DNI']; ?></h4></td>
+        <td width="400"  align="center"><h4><?php echo $student['Apellido'] . " " . $student['Nombre']; ?></h4></td>
+        <td idth="700" align="center"><h4></h4></td>
+        <td width="100" align="center" class="actions">
+          <BR>
+        <button class="quitarBtn" data-alumno-id=<?php echo $student['IdAlumno']; ?>>Quitar</button>
+        </td>
+      </tr>
+    <?php endforeach;
+
+		}
+  }
+?>
--- inscripciones/ListadoAlumnosPorMateria.php
+++ inscripciones/ListadoAlumnosPorMateria.php
@@ -55,129 +55,136 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-function getSubject($id) {
-  $query = "select * from terciario.materias_plan where IdMateria={$id}";
-  $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
-  $subject = mysqli_fetch_assoc($recordset);
-  return $subject;
-}
+// function getSubject($id) {
+//   $query = "select * from terciario.materias_plan where IdMateria={$id}";
+//   $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
+//   $subject = mysqli_fetch_assoc($recordset);
+//   return $subject;
+// }
+//
+// function getSubjectDetails($id) {
+//   $query = "select * from terciario.materias where IdMateria={$id}";
+//   $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
+//   $subjectDetails = mysqli_fetch_assoc($recordset);
+//   return $subjectDetails;
+// }
+//
+// function getSubjectCorrelatives($subject) {
+//   $correlatives = [];
+//
+//   $query = "select * from terciario.correlativas
+//             where (IdMateriaPlan={$subject['IdMateriaPlan']})";
+//   $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
+//   $result = mysqli_fetch_all($recordset, MYSQLI_ASSOC);
+//
+//   foreach($result as $correlative_result) {
+//     array_push($correlatives, getSubject($correlative_result['IdCorrelativa']));
+//   }
+//
+//   return $correlatives;
+// }
+//
+// function getStudents() {
+//   $query_students = "select Apellido, Nombre, DNI, IdAlumno from terciario.alumnos";
+//   $recordset_students = mysqli_query(dbconnect(),$query_students) or die(mysqli_error());
+//   $students = mysqli_fetch_all($recordset_students, MYSQLI_ASSOC);
+//
+//   return $students;
+// }
+//
+// function studentHasSign($student, $subject) {
+//   $hasSign = False;
+//
+//   $query = "select * from terciario.alumno_materias
+//             where (IdAlumno={$student['IdAlumno']} and IdMateriaPlan={$subject['IdMateriaPlan']})";
+//   $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
+//   $result = mysqli_fetch_assoc($recordset);
+//
+//   if (isset($result['FechaFirma'])) {
+//     $hasSign = True;
+//   }
+//
+//   return $hasSign;
+// }
+//
+// function studentHasCorrelatives($student, $subject_correlatives) {
+//   $has_correlatives = True;
+//
+//   foreach($subject_correlatives as $correlative) {
+//     if (!studentHasSign($student, $correlative)) {
+//       $has_correlatives = False;
+//       break;
+//     }
+//   }
+//
+//   return $has_correlatives;
+//   }
+//
+ $materia_id = $_POST['materia'];
+// $subject = getSubject($materia_id);
+// $subject_correlatives = getSubjectCorrelatives($subject);
+//
+// if(!isset($_POST['listado'])) {
+//   $allowed_student =[];
+//
+//   foreach (getStudents() as $student) {
+//     if (!studentHasSign($student, $subject) and
+//       studentHasCorrelatives($student, $subject_correlatives)) {
+//
+//       array_push($allowed_student, $student);
+//     }
+//   }
+// }
+//
+// if(isset($_POST['listado'])) {
+//   $allowed_student = json_decode($_POST['listado'], true);
+// }
+//
+// function DeleteAlumnoFromResult($listado, $id) {
+//   foreach ($listado as $key => $val) {
+//     if ($val['IdAlumno'] === $id) {
+//       unset($listado[$key]);
+//     }
+//   }
+//   return $listado;
+// }
+// if(isset($_POST['id'])) {
+//   $allowed_student = DeleteAlumnoFromResult($allowed_student,$_POST['id']);
+// }
+//
+// function AddAlumnoFromResult($listado, $student) {
+//   $allowed_students = json_decode($_POST['listado'], true);
+//   $student_in_allowed_students = FALSE;
+//
+//   // print_r($allowed_students); print_r($student);
+//   foreach($allowed_students as $allowed_student) {
+//     if ($allowed_student["IdAlumno"] == $student["IdAlumno"]) {
+//       $student_in_allowed_students = TRUE;
+//     }
+//   }
+//
+//   if (!$student_in_allowed_students) {
+//     $allowed_students[] = $student;
+//   }
+//
+//   return $allowed_students;
+// }
+//
+// if(isset($_POST['agregarAlumno'])) {;
+//   $student = json_decode($_POST['agregarAlumno'], true);
+//
+//   $allowed_student = AddAlumnoFromResult($allowed_student, $student);
+// }
+// #print_r($allowed_student);
 
+// obtengo los datos de la materia solamente
+ $subjectDetails = getSubjectDetails($materia_id);
 function getSubjectDetails($id) {
   $query = "select * from terciario.materias where IdMateria={$id}";
   $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error(dbconnect()));
   $subjectDetails = mysqli_fetch_assoc($recordset);
   return $subjectDetails;
 }
-
-function getSubjectCorrelatives($subject) {
-  $correlatives = [];
-
-  $query = "select * from terciario.correlativas
-            where (IdMateriaPlan={$subject['IdMateriaPlan']})";
-  $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
-  $result = mysqli_fetch_all($recordset, MYSQLI_ASSOC);
-
-  foreach($result as $correlative_result) {
-    array_push($correlatives, getSubject($correlative_result['IdCorrelativa']));
-  }
-
-  return $correlatives;
-}
-
-function getStudents() {
-  $query_students = "select Apellido, Nombre, DNI, IdAlumno from terciario.alumnos";
-  $recordset_students = mysqli_query(dbconnect(),$query_students) or die(mysqli_error());
-  $students = mysqli_fetch_all($recordset_students, MYSQLI_ASSOC);
-
-  return $students;
-}
-
-function studentHasSign($student, $subject) {
-  $hasSign = False;
-
-  $query = "select * from terciario.alumno_materias
-            where (IdAlumno={$student['IdAlumno']} and IdMateriaPlan={$subject['IdMateriaPlan']})";
-  $recordset = mysqli_query(dbconnect(), $query) or die(mysqli_error());
-  $result = mysqli_fetch_assoc($recordset);
-
-  if (isset($result['FechaFirma'])) {
-    $hasSign = True;
-  }
-
-  return $hasSign;
-}
-
-function studentHasCorrelatives($student, $subject_correlatives) {
-  $has_correlatives = True;
-
-  foreach($subject_correlatives as $correlative) {
-    if (!studentHasSign($student, $correlative)) {
-      $has_correlatives = False;
-      break;
-    }
-  }
-
-  return $has_correlatives;
-  }
-
-$materia_id = $_POST['materia'];
-$subject = getSubject($materia_id);
-$subject_correlatives = getSubjectCorrelatives($subject);
-
-if(!isset($_POST['listado'])) {
-  $allowed_student =[];
-
-  foreach (getStudents() as $student) {
-    if (!studentHasSign($student, $subject) and
-      studentHasCorrelatives($student, $subject_correlatives)) {
-
-      array_push($allowed_student, $student);
-    }
-  }
-}
-
-if(isset($_POST['listado'])) {
-  $allowed_student = json_decode($_POST['listado'], true);
-}
-
-function DeleteAlumnoFromResult($listado, $id) {
-  foreach ($listado as $key => $val) {
-    if ($val['IdAlumno'] === $id) {
-      unset($listado[$key]);
-    }
-  }
-  return $listado;
-}
-if(isset($_POST['id'])) {
-  $allowed_student = DeleteAlumnoFromResult($allowed_student,$_POST['id']);
-}
-
-function AddAlumnoFromResult($listado, $student) {
-  $allowed_students = json_decode($_POST['listado'], true);
-  $student_in_allowed_students = FALSE;
-
-  // print_r($allowed_students); print_r($student);
-  foreach($allowed_students as $allowed_student) {
-    if ($allowed_student["IdAlumno"] == $student["IdAlumno"]) {
-      $student_in_allowed_students = TRUE;
-    }
-  }
-
-  if (!$student_in_allowed_students) {
-    $allowed_students[] = $student;
-  }
-
-  return $allowed_students;
-}
-
-if(isset($_POST['agregarAlumno'])) {;
-  $student = json_decode($_POST['agregarAlumno'], true);
-
-  $allowed_student = AddAlumnoFromResult($allowed_student, $student);
-}
-#print_r($allowed_student);
-$subjectDetails = getSubjectDetails($materia_id);
-
 ?>
 
 <html lang="es">
@@ -191,11 +198,11 @@ $subjectDetails = getSubjectDetails($materia_id);
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script  type="text/javascript" src="ABM_Modal/js/jquery.min.js"></script>
     <script src="ABM_Modal/js/bootstrap.min.js"></script>
-    <script src="ABM_Modal/js/bootstrap-select.min.js"></script>
 
   </head>
   <body>
     <?php include("ABM_Modal/modal/modal_ListadoAgregarAlumno.php");?>
+
     <div id="result">
 
       <table width="1000" border="1" align="center">
@@ -207,26 +214,17 @@ $subjectDetails = getSubjectDetails($materia_id);
         </tbody>
       </table>
       <table width="1103" border="1" align="center" style="margin-bottom: 80px;">
-        <tbody>
+        <thead>
           <tr>
-            <td width="100" align="center">DNI</td>
-            <td width="100" align="center">Apellido y Nombre</td>
+            <td width="150" align="center">DNI</td>
+            <td width="300" align="center">Apellido y Nombre</td>
             <td width="700" align="center"></td>
+            <td width="100" align="center"></td>
           </tr>
-        <?php asort($allowed_student);
-        foreach ($allowed_student as $student): ?>
-        <tr>
-          <td align="center"><h4><?php echo $student['DNI']; ?></h4></td>
-          <td align="center"><h4><?php echo $student['Apellido'] . " " . $student['Nombre']; ?></h4></td>
-          <td align="center"><h4></h4></td>
-          <td align="center" class="actions">
-            <BR>
-          <button class="quitarBtn" data-alumno-id=<?php echo $student['IdAlumno']; ?>>Quitar</button>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-      </tbody>
+        </thead>
+        <tbody id="listado"></tbody>
       </table>
+
       <div style="text-align:center; position: fixed; bottom: 0; background-color: #fff; left: 0; right: 0; padding-bottom: 10px;">
           <BR>
           <input type=button onClick="location.href='Direcciones.php'" value='Volver al menu principal'>
@@ -241,82 +239,51 @@ $subjectDetails = getSubjectDetails($materia_id);
     $(document).ready(function(){
       var alumnoAAgregar = {};
 
-      $('#dataAgregar').on('hide.bs.modal', function(e)
-    {
-      console.log('asasassas',  $(this).parent())
-        $(this).parent().trigger('reset');
-        $(this).find('#alumnos_busqueda').empty();
-        $(this).find('#datos_error').empty();
-    }) ;
+      $('#dataAgregar').on('hide.bs.modal', function(e){
+          console.log('asasassas',  $(this).parent())
+            $(this).parent().trigger('reset');
+            $(this).find('#alumnos_busqueda').empty();
+            $(this).find('#datos_error').empty();
+            $('.modal-backdrop').remove();
+        }) ;
 
-      // eliminar alumno de la lista x id
-      $('.quitarBtn').each(function () {
+      //obtengo listado inicial de alumnos que pueden cursar
+      $.ajax({
+  			url:'./ABM_Modal/ajax/alumnosListado_ajax.php',
+  			data: {'action': 'ajax', 'materia': <?php echo $materia_id ?>},
+  			success:function(data){
+  				$("#listado").html(data);
+  			}
+  		});
+
+      //eliminar alumno de la lista x id
+      $(document).on("click",".quitarBtn",function(){
         const $this = $(this);
-        $this.on('click', function(){
-          const studentId = $this.attr('data-alumno-id');
-          $.ajax({
-             url: 'ListadoAlumnosPorMateria.php',
-             type: 'POST',
-             data: 'id='+ studentId + '&materia=<?php echo $materia_id ?>' + '&listado=<?php echo json_encode($allowed_student) ?>',
-             success: function(data) {
-               $('#result').html(data);
-             }
-           });
+        const studentId = $this.attr('data-alumno-id');
+        //console.log($this, studentId)
+        $.ajax({
+           url: './ABM_Modal/ajax/alumnosListado_ajax.php',
+           type: 'POST',
+           data: {'action': 'borrar', 'materia': <?php echo $materia_id ?>, 'id': studentId},
+           success: function(data) {
+             $("#listado").html(data);
+           }
          });
-       });
+      });
 
       // buscar alumno por dni en el modal
       $('#dataAgregar').on('show.bs.modal', function (event) {
-  		  var modal = $(this);
-
-  		  modal.find('.modal-title').text('Buscar alumno por DNI ');
-
+        console.log(event)
+        var modal = $(this);
+        modal.find('.modal-title').text('Buscar alumno por DNI ');
         const submitButton = modal.find('.btn-primary');
         modal.find('#materia_id').val(<?php echo $materia_id ?>);
-
-        const listadoAlumnosJS = <?php echo json_encode($allowed_student) ?>;
-        let sarasa;
-        let error = false;
+        //const listadoAlumnosJS = <?php //echo json_encode($allowed_student) ?>;
 
         var DNI = modal.find('#dniBuscar');
-        let Agregarerror = modal.find('#datos_error');
 
         $(DNI).on('change', function() {
           var valorABuscar = DNI.val();
-
-          // chequeo que el dni a agregar no exista en el listado
-          for( const alumno in Object.values(listadoAlumnosJS)) {
-            sarasa = Object.values(listadoAlumnosJS[alumno]).filter(pis => {
-            //  console.log(pis === valorABuscar, pis, valorABuscar)
-              return pis === valorABuscar
-            });
-
-            //console.log(sarasa)
-
-            if(sarasa.length > 0) {
-              const data = '<div class="alert alert-warning alert-dismissable">Alumno ya se encuentra en la lista</div>'
-              $('#datos_error').html(data);
-              error = true;
-              break;
-            } else if(error) {
-              console.log('asasas')
-              error = false;
-            }
-          }
-
-          if(error) {
-            submitButton.attr('disabled', 'disabled');
-          } else {
-            submitButton.removeAttr('disabled');
-            $('#datos_error').empty();
-          }
-
-          if(valorABuscar === '') {
-            Agregarerror.empty();
-            if(error) {
-              submitButton.removeAttr('disabled');
-            }
-          }
 
           $.ajax({
              url: 'ABM_Modal/ajax/agregarAlumnosListado_ajax.php',
@@ -333,25 +300,12 @@ $subjectDetails = getSubjectDetails($materia_id);
              }
            });
         });
-  		});
-
-      // agregar el alumno a la lista
-      $('#actualidarDatos').submit(function( event ) {
-        event.preventDefault();
-        var modal = $(this);
-
-        $.ajax({
-          type: "POST",
-          url: "ListadoAlumnosPorMateria.php",
-          data: 'agregarAlumno=' + JSON.stringify(alumnoAAgregar) + '&materia=<?php echo $materia_id ?>' + '&listado=<?php echo json_encode($allowed_student) ?>',
-          success: function(data){
-            $("#result").html(data);
-          }
-        });
-
-        modal.find('#datos_error').html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>¡Bien hecho!</strong></div>')
-
       });
+
+      //falta hacer chequeo de si esta en la lista y mostrar error del lado de <?php  ?>
+      //falta accion de botton agregar (dado que no haya error antes)
+
+
     });
 
     </script>
