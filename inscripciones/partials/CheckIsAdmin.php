8fa36ec45ba3496bc710cc3918dbf9ac9274072e
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-06-15 15:01:22 -0300

    Agrego partial para chequear si es admin

---
+++ inscripciones/partials/CheckIsAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// verify that the user is admin 
+if ($_SESSION['MM_UserGroup'] != 'Admin') {
+    die("No cuenta con permisos suficientes");
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
