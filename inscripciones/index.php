1b5aca66c170f041f69b7487bdb7f7ad30fabce1
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-10-15 22:17:15 -0300

    Agrego créditos :poop:

--- inscripciones/index.php
+++ inscripciones/index.php
@@ -46,5 +46,9 @@
     <script src="/scripts/jquery.slim.min.js"></script>
     <script src="/scripts/popper-1.14.7.min.js"></script>
     <script src="/scripts/bootstrap.min.js"></script>
+    <script>
+      console.log('%c Creado por los alumnos del IFTS! ', 'background: #ff0000; color: #1500ff; font-size: 20px;');
+      console.log('%c Última modificación: 2019 - Por Cris Montes de Oca, Sebas Poliak, Mati Rojas y Agus Tashdjian! ', 'background: #ff0000; color: #1500ff; font-size: 15px;');
+    </script>
   </body>
 </html>
