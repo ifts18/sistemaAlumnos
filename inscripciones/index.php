57dc40207f43d25aca0ff255ad81d24af3b944bf
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-10-15 21:24:09 -0300

    Uso clases nuevas

--- inscripciones/index.php
+++ inscripciones/index.php
@@ -1,9 +1,12 @@
 <?php
+  include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/db.php');
   include_once($_SERVER['DOCUMENT_ROOT'].'/config/routes.php');
   include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/auth.php');
+  include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/sessions.php');
+  include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/page.php');
 
   
-  if ($requestRoute !== 'login' && !isLoggedIn()) {
+  if ($requestRoute !== 'login' && !$AuthManager->isLoggedIn()) {
     header('Location: /login');
   }
 ?>
@@ -22,10 +25,10 @@
     <title>Sistema de alumnos</title>
   </head>
   <body>
-    <?php $requestRoute !== 'login' ? include_once($_SERVER['DOCUMENT_ROOT'].'/blocks/navbar.php') : ''; ?>
+    <?php exclude_blocks($requestRoute) ? '' : include_once($_SERVER['DOCUMENT_ROOT'].'/blocks/navbar.php'); ?>
     <div class="container-fluid">
       <div class="row">
-        <?php $requestRoute !== 'login' ? include_once($_SERVER['DOCUMENT_ROOT'].'/blocks/sidebar.php') : ''; ?>
+        <?php exclude_blocks($requestRoute) ? '' : include_once($_SERVER['DOCUMENT_ROOT'].'/blocks/sidebar.php'); ?>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap flex-column align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2"><?php echo $routes[$requestRoute]['name']; ?></h1>
