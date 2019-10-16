307fc2d18eeb55187eee7273985c8964a46ae794
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-10-15 21:23:02 -0300

    Hago clase de authenticacion

--- inscripciones/helpers/auth.php
+++ inscripciones/helpers/auth.php
@@ -1,23 +1,54 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/sessions.php');
 
-function isLoggedIn() {
-  start_session();
-  return null !== get('user_id');
+include_once($_SERVER['DOCUMENT_ROOT'].'/helpers/db.php');
+
+class AuthManager {
+  public function __construct($DbManager) {
+    $this->sessionKey = 'user_id';
+    $this->dbManager = $DbManager;
+  }
+
+  public function isLoggedIn() {
+    start_session();
+    return null !== get($this->sessionKey);
+  }
+  
+  public function doLogin($dni, $password) {
+    start_session();
+    $escapedDni = $this->dbManager->quote($dni);
+    $escapedPassword = $this->dbManager->quote($password);
+
+    $alumnos = $this->dbManager->select('SELECT * FROM alumnos WHERE DNI = '.$escapedDni.' AND Password = '.$escapedPassword);
+    $admin = $this->dbManager->select('SELECT * FROM admin WHERE DNI = '.$escapedDni.' AND Password = '.$escapedPassword);
+
+    if (empty($alumnos) && empty($admin)) {
+      return false;
+    }
+
+    // Negrada, porque la base tiene DOS tablas (una admin y otra de alumnos) -> No queremos tocar la base en esta version
+    if (!empty($alumnos)) {
+      $final = $alumnos[0];
+    }
+
+    if (!empty($admin)) {
+      $final = $admin[0];
+    }
+
+    set($this->sessionKey, $final);
+
+    return true;
+  }
+  
+  public function logout() {
+    start_session();
+    if (has($this->sessionKey)) {
+      remove($this->sessionKey);
+    }
+  }
+  
+  public function getUserSession() {
+    return get($this->sessionKey);
+  }
 }
 
-function login($user, $password) {
-  // TODO:
-  // 1. Ir a la base y buscar el usuario
-  // 1.1 Sino existe, devolver error y salir
-  // 1.2 Si existe, ir a la base y comparar con la password del usuario
-  // 2. Si esta todo bien, guardamos la info del usuario en sesion y nos vamos
-  // 2.1 Sino, volvemos al login
-  // QUITAR ESTE MOCK!!!
-  start_session();
-  set('user_id', $user);
-}
-
-function getUsername() {
-  return get('user_id');
-}
+$AuthManager = new AuthManager($DbManager);
