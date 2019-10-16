b2c3ee70ebf59bee9bbf4f9f5a3961723ef860a3
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-10-16 00:07:29 -0300

    Uso form helper, ejemplo.

--- inscripciones/pages/alta-alumnos.php
+++ inscripciones/pages/alta-alumnos.php
@@ -1,7 +1,17 @@
+<?php 
+$form_fields = [
+  [
+    'id' => 'name',
+    'label' => 'Nombre',
+    'type' => 'text',
+    'value' => isset($_POST['name']) ? $_POST['name'] : ''
+  ]
+];
+?>
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12">
-      <h2></h2>
+      <?php generate($form_fields, ''); ?>
     </div>
   </div>
 </div>
