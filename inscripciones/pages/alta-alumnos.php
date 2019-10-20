6c25e280df9860c9c0affcca623520f0b1e483ae
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2019-10-20 00:19:07 -0300

    Agrego campos del formulario

--- inscripciones/pages/alta-alumnos.php
+++ inscripciones/pages/alta-alumnos.php
@@ -5,13 +5,32 @@ $form_fields = [
     'label' => 'Nombre',
     'type' => 'text',
     'value' => isset($_POST['name']) ? $_POST['name'] : ''
+  ],
+  [
+    'id' => 'lastname',
+    'label' => 'Apellido',
+    'type' => 'text',
+    'value' => isset($_POST['lastname']) ? $_POST['lastname'] : ''
+  ],
+  [
+    'id' => 'mail',
+    'label' => 'Mail',
+    'type' => 'text',
+    'value' => isset($_POST['mail']) ? $_POST['mail'] : ''
+  ],
+  [
+    'id' => 'dni',
+    'label' => 'DNI',
+    'type' => 'text',
+    'value' => isset($_POST['dni']) ? $_POST['dni'] : ''
   ]
 ];
 ?>
+
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12">
-      <?php generate($form_fields, ''); ?>
+      <?php generate($form_fields, '', 'alta-alumno'); ?>
     </div>
   </div>
 </div>
