092043329174ad87abbc0e6d26e4d7d957f67d87
Sebastián Poliak <sebastianl.poliak@gmail.com>
2019-11-11 17:35:12 -0300

    Feature/agregar fecha desde y hasta (#30)
    * Agrego valores a la tabla. Agrego valores al modal
    
    * Agrego datepicker
    
    * Cambio query. Agrego datepicker. Validaciones del lado del cliente. Etc
    
    * Cambio query para recibir materia-division

--- inscripciones/AltaMesa1.php
+++ inscripciones/AltaMesa1.php
@@ -19,7 +19,18 @@ if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
 ?>
 
 <?php
-$recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FROM materias");
+$recordsetMaterias = mysqli_query(dbconnect(), "
+  SELECT M.IdMateria, M.Descripcion, D.IdDivision, D.NombreDivision
+  FROM materias_plan MP
+  INNER JOIN materias M ON M.IdMateria = MP.IdMateria
+  INNER JOIN materias_division MD ON MD.IdMateriaPlan = M.IdMateria
+  INNER JOIN division D ON D.IdDivision = MD.IdDivision
+  UNION
+  SELECT M.IdMateria, M.Descripcion, '0', 'Ninguna'
+  FROM materias M
+  WHERE M.IdMateria > 11;
+");
+// El 11 es porque es la ultima materia de 1r año y en el 2019 solo 1r año tenia A y B, suerte para el 2020
 ?>
 
 <html>
@@ -30,6 +41,7 @@ $recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FR
 	  <link href="ABM_Modal/css/bootstrap.min.css" rel="stylesheet">
     <link href="ABM_Modal/css/dataTables.bootstrap.css" rel="stylesheet">
     <link href="ABM_Modal/css/bootstrap-select.min.css" rel="stylesheet">
+    <link href="ABM_Modal/css/datepicker.min.css" rel="stylesheet">
     <title>Nueva mesa de final</title>
     <style>
       .footer {
@@ -59,40 +71,22 @@ $recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FR
       </div>
       <div class="row" style="margin-top: 30px;">
         <div class="col-xs-12">
-          <form action="insertMesa1.php" method="Post" id="add-mesa" onsubmit="return confirm('¿Seguro quiere guardar los datos?');">
+          <form action="insertMesa1.php" method="Post" id="add-mesa">
             <div class="container">
               <div class="row">
                 <div class="col-xs-12">
                   <div class="form-inline">
                     <div class="form-group">
                       <label for="fechamesa" style="margin-right: 5px;">Fecha de la mesa</label>
-                      <input type="text" class="form-control" name="fechamesa" id="fechamesa">
+                      <input type="text" class="form-control pone-calendar" name="fechamesa" id="fechamesa">
                     </div>
                     <div class="form-group">
                       <label for="desde" style="margin-right: 5px;">Disponible desde</label>
-                      <input type="text" class="form-control" name="desde" id="desde">
+                      <input type="text" class="form-control pone-calendar" name="desde" id="desde">
                     </div>
                     <div class="form-group">
                       <label for="hasta" style="margin-right: 5px;">Disponible hasta</label>
-                      <input type="text" class="form-control" name="hasta" id="hasta">
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row" style="margin-top: 30px;">
-                <div class="col-xs-12">
-                  <div class="form-inline">
-                    <label style="margin-right: 5px;">División</label>
-                    <div class="form-group">
-                      <label class="radio-inline">
-                        <input type="radio" name="division" value="1"> A
-                      </label>
-                      <label class="radio-inline">
-                        <input type="radio" name="division" value="2"> B
-                      </label>
-                      <label class="radio-inline">
-                        <input type="radio" name="division" value="0"> Ninguna
-                      </label>
+                      <input type="text" class="form-control pone-calendar" name="hasta" id="hasta">
                     </div>
                   </div>
                 </div>
@@ -103,13 +97,13 @@ $recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FR
                     <label style="margin-right: 5px;">Turno</label>
                     <div class="form-group">
                       <label class="radio-inline">
-                        <input type="radio" name="turno" value="1"> 1
+                        <input type="radio" name="turno" value="1"> 1 - Principio de año
                       </label>
                       <label class="radio-inline">
-                        <input type="radio" name="turno" value="2"> 2
+                        <input type="radio" name="turno" value="2"> 2 - Mitad de año
                       </label>
                       <label class="radio-inline">
-                        <input type="radio" name="turno" value="3"> 3
+                        <input type="radio" name="turno" value="3"> 3 - Fin de año
                       </label>
                     </div>
                   </div>
@@ -120,12 +114,23 @@ $recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FR
                   <div class="form-group">
                     <label for="materia">Materias</label>
                     <table class="table">
-                      <?php while ($rowRecordset = mysqli_fetch_assoc($recordsetMaterias)) { ?>
+                    <?php 
+                        while ($rowRecordset = mysqli_fetch_assoc($recordsetMaterias)) {
+                          $descripcion = $rowRecordset['Descripcion'];
+                          if ($rowRecordset['IdDivision'] !== '0') {
+                            $descripcion = $descripcion . ' - División: '.$rowRecordset['NombreDivision'];
+                          }
+                        ?>
                         <tr>
-                          <td><?php echo $rowRecordset['Descripcion']; ?></td>
+                          <td><?php echo $descripcion; ?></td>
                           <td>
                             <label class="checkbox-inline">
-                              <input type="checkbox" name="materias[]" value="<?php echo $rowRecordset['IdMateria']; ?>">
+                              <?php 
+                                if ($rowRecordset['IdDivision'] !== '0') { ?>
+                                  <input type="checkbox" data-materia="<?php echo $rowRecordset['IdMateria']; ?>" data-division="<?php echo $rowRecordset['IdDivision']; ?>" class="with-division" name="materias[]" value="<?php echo $rowRecordset['IdMateria'].'-'.$rowRecordset['IdDivision']; ?>">
+                                <?php } else { ?>
+                                  <input type="checkbox" name="materias[]" value="<?php echo $rowRecordset['IdMateria'].'-0'; ?>">
+                              <?php } ?>
                             </label>
                           </td>
                         </tr>
@@ -150,8 +155,52 @@ $recordsetMaterias = mysqli_query(dbconnect(), "SELECT IdMateria, Descripcion FR
       </div>
     </div>
     <script src="ABM_Modal/js/jquery.min.js"></script>
+    <script src="ABM_Modal/js/datepicker.min.js"></script>
+    <script src="ABM_Modal/js/datepicker.es.min.js"></script>
     <script>
+      function oneYearAfter() {
+        const da = new Date();
+        return new Date(da.getFullYear() + 1, da.getMonth(), da.getDate());
+      }
+
+      $('.pone-calendar').datepicker({
+        format: "dd/mm/yyyy",
+        language: "es",
+        daysOfWeekDisabled: "0,6",
+        autoclose: true,
+        startDate: new Date(),
+        endDate: oneYearAfter()
+      });
+
       $('#button-find').click(e => { e.preventDefault; $('#add-mesa').submit(); });
+
+      $('#add-mesa').submit(e => {
+        e.preventDefault();
+        const form = e.target;
+        const serialized = $(form).serializeArray();
+
+        if (serialized[0].name === 'fechamesa' && serialized[0].value === '') {
+          return alert('Debe ingresar una fecha valida para la mesa');
+        }
+
+        if (serialized[1].name === 'desde' && serialized[1].value === '') {
+          return alert('Debe ingresar una fecha valida para el inicio del periodo de inscripcion');
+        }
+
+        if (serialized[2].name === 'hasta' && serialized[2].value === '') {
+          return alert('Debe ingresar una fecha valida para el fin del periodo de inscripcion');
+        }
+
+        if (serialized.findIndex(e => e.name === 'turno') === -1) {
+          return alert('Debe seleccionar un turno valido');
+        }
+
+        if (serialized.findIndex(e => e.name === 'materias[]') === -1) {
+          return alert('Debe seleccionar al menos una materia');
+        }
+
+        form.submit();
+      });
     </script>
   </body>
 </html>
--- inscripciones/insertMesa1.php
+++ inscripciones/insertMesa1.php
@@ -11,31 +11,15 @@ if (isset($_GET['accesscheck'])) {
   $_SESSION['PrevUrl'] = $_GET['accesscheck'];
 }
 
-if (isset($_POST['turno']) == '') {
-	return header('Location altamesa.php');
-}
+$format = '%d/%m/%Y : %H:%i';
 
-for ($i = 0; $i < count($_POST["materias"]); $i++) {
-	$turno = $_POST["turno"];
-	$division = isset($_POST["division"]) ? $_POST["division"] : 0;
-	$materiaId = $_POST["materias"][$i];
-	$fechamesa= $_POST['fechamesa'];
+for ($i = 0; $i < count($_POST['materias']); $i++) {
+	$turno = $_POST['turno'];
+	$fechamesa = $_POST['fechamesa'];
 	$desde = $_POST['desde'];
 	$hasta = $_POST['hasta'];
-
-	mysqli_query(dbconnect(), "
-		INSERT INTO mesas_final
-		(IdMesaFinal, IdTurnosFinales, IdDivision, IdMateriaPlan, Abierta, FechaMesa, Limite, 
-		FechaCreacion, DisponibleDesdeFecha, DisponibleHastaFecha)
-		VALUES (NULL, $turno, $division, $materiaId, 1, 
-		STR_TO_DATE('$fechamesa', '%d/%m/%Y %H:%i'), 
-		0, 
-		CURRENT_TIMESTAMP, 
-		STR_TO_DATE('$desde', '%d/%m/%Y %H:%i'), 
-		STR_TO_DATE('$hasta', '%d/%m/%Y %H:%i')
-		);
-	");
+	list($idMateria, $idDivision) = explode('-', $_POST['materias'][$i]);
+	mysqli_query(dbconnect(), "INSERT INTO mesas_final (IdMesaFinal, IdTurnosFinales, IdDivision, IdMateriaPlan, Abierta, FechaMesa, Limite, FechaCreacion, DisponibleDesdeFecha, DisponibleHastaFecha) VALUES (NULL, $turno, $idDivision, $idMateria, 1, STR_TO_DATE('$fechamesa', '$format'), 0, CURRENT_TIMESTAMP, STR_TO_DATE('$desde', '$format'), STR_TO_DATE('$hasta', '$format'));");
 }
 
-header("Location: " . "ABM_Modal/MesasDeFinales.php" );
-?>
+header('Location: ABM_Modal/MesasDeFinales.php');
